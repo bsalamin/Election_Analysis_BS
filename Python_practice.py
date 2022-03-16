b90c98a3b5096970841e672d2b52eb75c1f32dca
Ben Salamin <bensalamin@MacBook-Pro.local>
2022-03-16 12:52:39 -0400

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,8 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county, voters in voting_data.items():
+    print(f'{county} county has {voters:,} registered voters.')
